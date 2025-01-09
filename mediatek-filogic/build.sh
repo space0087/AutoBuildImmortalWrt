97d8688eec1ebc5bbf2d962cba52bba6960c09c5
space0087 <115089713+space0087@users.noreply.github.com>
2025-01-09 21:38:17 +0800

    Update build.sh

--- mediatek-filogic/build.sh
+++ mediatek-filogic/build.sh
@@ -15,7 +15,7 @@ PACKAGES="$PACKAGES luci-i18n-filebrowser-zh-cn"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
-#23.05
+23.05
 PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
